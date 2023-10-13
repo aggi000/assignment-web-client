7072e740b746fa256d9606af08c82db9d9e5af12
Agrim Sood <agrimsood@gmail.com>
2023-10-13 15:35:41 -0600

    initial commit

--- httpclient.py
+++ httpclient.py
@@ -33,21 +33,23 @@ class HTTPResponse(object):
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
-
-    def connect(self, host, port):
+    
+    def connect(self, host, port=80):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((host, port))
         return None
 
     def get_code(self, data):
-        return None
+        try:
+            return int(data.split(' ')[1])
+        except:
+            return None
 
-    def get_headers(self,data):
-        return None
+    def get_headers(self, data):
+        return data.split('\r\n\r\n')[0]
 
     def get_body(self, data):
-        return None
+        return data.split('\r\n\r\n')[1]
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -55,34 +57,67 @@ class HTTPClient(object):
     def close(self):
         self.socket.close()
 
-    # read everything from the socket
     def recvall(self, sock):
         buffer = bytearray()
         done = False
         while not done:
             part = sock.recv(1024)
-            if (part):
+            if part:
                 buffer.extend(part)
             else:
                 done = not part
-        return buffer.decode('utf-8')
+        return buffer.decode('utf-8', errors='ignore')
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
-        return HTTPResponse(code, body)
+       parsed_url = urllib.parse.urlparse(url)
+       self.connect(parsed_url.hostname, parsed_url.port)#connecting to the server
+       request_line = f"GET {parsed_url.path} HTTP/1.1\r\n"#building http request
+       headers = f"Host: {parsed_url.hostname}\r\n\r\n"
+       self.sendall(request_line + headers)#sending the request
+       response_data = self.recvall(self.socket)#recieving the request
+       code = self.get_code(response_data)#assigning response to code and body
+       body = self.get_body(response_data)
+       self.close()
+       return HTTPResponse(code, body)
+
+  
+
+
+    
 
     def POST(self, url, args=None):
-        code = 500
+        parsed_url = urllib.parse.urlparse(url)
+    
+        self.connect(parsed_url.hostname, parsed_url.port)#tcp connection with server
+        
+        content_type = "application/x-www-form-urlencoded"#default for submitting post
         body = ""
+        if args is not None:
+            body = urllib.parse.urlencode(args)#converts to ASCII string 
+        
+        content_length = len(body)
+        
+        request_line = f"POST {parsed_url.path} HTTP/1.1\r\n"#building http request
+        headers = f"Host: {parsed_url.hostname}\r\n"
+        headers += f"Content-Type: {content_type}\r\n"
+        headers += f"Content-Length: {content_length}\r\n\r\n"
+        
+        self.sendall(request_line + headers + body)#sent to server
+        response_data = self.recvall(self.socket)#recieve the code from server
+        
+        code = self.get_code(response_data)
+        body = self.get_body(response_data)
+        
+        self.close()#close connection
         return HTTPResponse(code, body)
+
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
-            return self.POST( url, args )
+            return self.POST(url, args)
         else:
-            return self.GET( url, args )
-    
+            return self.GET(url, args)
+
 if __name__ == "__main__":
     client = HTTPClient()
     command = "GET"
@@ -90,6 +125,6 @@ if __name__ == "__main__":
         help()
         sys.exit(1)
     elif (len(sys.argv) == 3):
-        print(client.command( sys.argv[2], sys.argv[1] ))
+        print(client.command(sys.argv[2], sys.argv[1]))
     else:
-        print(client.command( sys.argv[1] ))
+        print(client.command(sys.argv[1]))
